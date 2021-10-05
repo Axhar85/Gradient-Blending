edfe074e663d81769f72500ac4889c2f409c2d8d
Axhar85 <azhararshad_ntu@yahoo.com>
2021-10-05 17:43:36 +0200

    fixed image poistion

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
  // Create first objest of Granim
 
  let granimInstance = new Granim({
-     element: "#canvas-image-blending",
-     direction: 'top-bottom',
-     isPausedWhenNotInView: true,
-     Image: {
-         source: "./assets/img1.jpg",
-         blendingMode:'multiply',
-         stretchMode : ['stretch', 'stretch-if-bigger']
-     },
+    element : "#canvas-image-blending",
+    direction : 'top-bottom',
+    isPausedWhenNotInView : true,
+    image : {
+        source : './assets/img1.jpg',
+        blendingMode: 'multiply',
+        stretchMode : ['stretch', 'stretch-if-bigger']
+    },
      states:{
          "default-state":{
             gradients:[
