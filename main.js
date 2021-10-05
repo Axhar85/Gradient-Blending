1715928c79bc71c0baf2fb5a5917d4e6034b92c3
Axhar85 <azhararshad_ntu@yahoo.com>
2021-10-05 17:32:49 +0200

    created onclick funtion for morning, evening and night

--- main.js
+++ main.js
@@ -12,12 +12,49 @@
      states:{
          "default-state":{
             gradients:[
-                ['#DAE2F8','#DAE2F8'],
-                ['#e35d5b','#f0cb35'],
-                ['#0F2027', '#8E9EAB']
+                  ['#DAE2F8','#DAE2F8'],
+               // ['#e35d5b','#f0cb35'],
+                //['#0F2027', '#8E9EAB']
             ],
             transitionSpeed: 4000,
-            loop: true,
+            loop: false,
+         },
+         "evening-state":{
+            gradients:[
+                //['#DAE2F8','#DAE2F8'],
+                  ['#e35d5b','#f0cb35'],
+                //['#0F2027', '#8E9EAB']
+            ],
+            transitionSpeed: 4000,
+            loop: false,
+         },
+         "dark-state":{
+            gradients:[
+                //['#DAE2F8','#DAE2F8'],
+                //['#e35d5b','#f0cb35'],
+                  ['#0F2027', '#8E9EAB']
+            ],
+            transitionSpeed: 4000,
+            loop: false,
          }
      }
  })
+
+ const morning = document.getElementById("morning")
+ const evening = document.getElementById("evening")
+ const night = document.getElementById("night")
+
+ // onclick morning
+ morning.addEventListener("click", function(event){
+     granimInstance.changeState('default-state')
+ })
+
+  // onclick evening
+  evening.addEventListener("click", function(event){
+    granimInstance.changeState('evening-state')
+})
+
+// onclick night
+night.addEventListener("click", function(event){
+    granimInstance.changeState('dark-state')
+})
